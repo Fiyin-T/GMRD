4bc5430e169acb58985be25e1cf31cca514fc623
Fiyin <fiyin@MacBook.local>
2023-03-27 15:04:54 -0600

    imported User module

--- main_app/models.py
+++ main_app/models.py
@@ -1,19 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=100)
     release_date = models.DateField()
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=2000)
 
     def __str__(self):
         return self.title
-
-class User(models.Model):
-    username = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.username
 
 class List(models.Model):
     name = models.CharField(max_length=100)
