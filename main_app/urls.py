8ace7ed42db8c160f31893c3e8ca12f3029b877b
Michael Lucky <luckymichael00@gmail.com>
2023-03-27 11:08:05 -0400

    small update

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('accounts/signup/',views.signup, name='signup'),
     path('', views.home, name='home'),
     path('games/genres/', views.game_genres, name='game_genres'),
+    # path('games/genre/', views.game_search, name=''),
+    # path('games/search/', views.game_search, name='search'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -23,12 +23,11 @@ def signup(request):
 def home(request):
   return render(request, 'home.html')
 
-
 def game_genres(request):
   # games api stuff here
   url = 'https://api.rawg.io/api/genres?key={}'
   api_key = os.environ.get('API_KEY')
   game_data = requests.get(url.format(api_key)).json()
   genres = game_data['results']
-  return render(request, 'games.html', { 'genres': genres, 'game_data':game_data})
+  return render(request, 'games/genres.html', { 'genres': genres, 'game_data':game_data})
   
