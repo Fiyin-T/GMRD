73d5042c88b837b2be60dd634d628c52b6431bef
Fiyin <fiyin@MacBook.local>
2023-03-28 08:31:37 -0600

    list routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('list/<int:list_id>/assoc_game/<int:game_id>/', views.assoc_game, name='assoc_game'),
     path('games/genre/<slug:genre>/', views.genre_index, name='genre_index'),
     path('games/<int:id>/', views.game_index, name='game_index'),
+    path('lists/', views.lists_index, name='index'),
+    path('lists/create/', views.ListCreate.as_view(), name='lists_create'),
+    path('lists/<int:list_id>/', views.lists_detail, name='list_detail')
 ]
